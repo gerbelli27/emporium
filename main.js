2e0eb42d8fce256eb8d333c9568011b78a1c72e5
gerbelli27 <guerbell@hotmail.com>
2022-06-09 17:50:29 +0100

    chore js

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ function closeMenuBody() {
   nav.classList.remove("active");
 }
 closeMenu.addEventListener("click", closeMenuBody);
+
+function menuFixed() {
+  const navToTop = nav.getBoundingClientRect().top;
+
+  if (navToTop < 0) {
+    btnMenu.classList.add('fixed')
+    nav.classList.add('fixed-menu')
+  } else {
+    btnMenu.classList.remove('fixed')
+    nav.classList.remove('fixed-menu')
+  }
+}
+
+window.addEventListener('scroll', menuFixed)
